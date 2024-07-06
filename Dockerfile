146466128804a81809dd590feb1c61ab1783ef9b
Yassine Dergaoui <bcha2825@hotmail.fr>
2024-07-06 17:45:05 +0200

    removed docker from root app

--- Dockerfile
+++
@@ -1,9 +0,0 @@
-FROM maven:3.9.0-eclipse-temurin-19-alpine AS build
-LABEL authors="yassine"
-WORKDIR /app
-
-COPY pom.xml .
-RUN mvn -B dependency:go-offline
-
-COPY src src
-RUN mvn -B package -DskipTests
--- backend/src/main/java/org/ichat/backend/controller/SocialController.java
+++ backend/src/main/java/org/ichat/backend/controller/SocialController.java
@@ -2,12 +2,9 @@ package org.ichat.backend.controller;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.ichat.backend.model.assemblers.PostModelAssembler;
 import org.ichat.backend.model.tables.social.Post;
 import org.ichat.backend.model.util.social.PostRequest;
 import org.ichat.backend.service.social.IPostService;
-import org.springframework.hateoas.EntityModel;
-import org.springframework.hateoas.IanaLinkRelations;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -19,14 +16,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/social")
 public class SocialController {
     private final IPostService postService;
-    private final PostModelAssembler assembler;
 
     @PostMapping("/post")
     public ResponseEntity<Post> createPost(@Valid @RequestBody PostRequest req) {
         Post post = postService.createPost(req);
-        EntityModel<Post> postModel = assembler.toModel(post);
-        return ResponseEntity.created(postModel.getRequiredLink(IanaLinkRelations.SELF).toUri())
-                .body(postModel.getContent());
+        return ResponseEntity.ok(post);
     }
 
 }
