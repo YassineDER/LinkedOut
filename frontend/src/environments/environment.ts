09b25b246e71e43bf895a85500e20040df161717
Yassine Dergaoui <bcha2825@hotmail.fr>
2024-07-08 18:26:52 +0200

    Fixed CORS in backend

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    hostUrl: 'https://backend:8080',
+    hostUrl: 'http://backend:8080',
     RECAPTCHA_SITE_KEY : "6Ld5duIpAAAAAN5aZxtsN9JscAckhTQ3h5jTvJyz",
 };
