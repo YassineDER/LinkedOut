39969a08649f5e24ee6cf1661c5e88e29c521786
Yassine Dergaoui <bcha2825@hotmail.fr>
2024-07-08 15:07:31 +0200

    Fixed CORS

--- backend/src/main/java/org/ichat/backend/config/SecurityConfiguration.java
+++ backend/src/main/java/org/ichat/backend/config/SecurityConfiguration.java
@@ -20,6 +20,7 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.beans.factory.annotation.Value;
 
 import java.util.UUID;
 
