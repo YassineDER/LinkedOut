820cbf045d34883f5ef15ecea64b82e275bba511
Yassine Dergaoui <bcha2825@hotmail.fr>
2024-07-10 19:40:09 +0200

    Changed containers names

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
-    hostUrl: 'https://linkedout_backend_1:8080',
+    hostUrl: 'https://backend:8080',
     RECAPTCHA_SITE_KEY : "6Ld5duIpAAAAAN5aZxtsN9JscAckhTQ3h5jTvJyz",
 };
