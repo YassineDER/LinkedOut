4106a36b247ef7a9ce7456becfe6195875ba1189
Yassine Dergaoui <bcha2825@hotmail.fr>
2024-07-11 17:18:02 +0200

    Deleted frontend container, will use the installed nginx on VM

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,6 +1,5 @@
 export const environment = {
     production: true,
-    // hostUrl: 'https://backend:8080',
-    hostUrl: 'https://backend:8080',
+    hostUrl: 'https://localhost:8080',
     RECAPTCHA_SITE_KEY : "6Ld5duIpAAAAAN5aZxtsN9JscAckhTQ3h5jTvJyz",
 };
