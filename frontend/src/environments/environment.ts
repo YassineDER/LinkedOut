b93a7253d7bda7bc75e5a49136ec1079f1e732c7
Yassine Dergaoui <bcha2825@hotmail.fr>
2024-07-11 14:54:36 +0200

    Debugging the fucking CORS errors

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
     production: true,
-    hostUrl: 'https://backend:8080',
+    // hostUrl: 'https://backend:8080',
+    hostUrl: 'https://yassine.onthewifi.com:8080',
     RECAPTCHA_SITE_KEY : "6Ld5duIpAAAAAN5aZxtsN9JscAckhTQ3h5jTvJyz",
 };
