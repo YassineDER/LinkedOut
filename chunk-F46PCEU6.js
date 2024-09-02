af11efec8dd5de6ef440586bc252f923bf7e5510
Heroku-Deploy <yassineyassine333@gmail.com>
2024-09-02 16:47:27 +0000

    Auto-generated commit
    Triggered by commit: https://github.com/YassineDER/LinkedOut/commit/b597007a1389225023aa57ff0405c17c79c2e4d3

---
+++ chunk-F46PCEU6.js
@@ -0,0 +1,1 @@
+import{C as n,F as o,fb as s}from"./chunk-TYZMNGUD.js";var f=(()=>{let t=class t{constructor(r){this.http=r}isConnection(r,i){return!1}triggerMsg(r){}connect(r){}};t.\u0275fac=function(i){return new(i||t)(o(s))},t.\u0275prov=n({token:t,factory:t.\u0275fac});let e=t;return e})();export{f as a};
